a4d11fa7e9d86aca20925350e82119070b1a1684
Seth Keel <seth.keel@gmail.com>
2016-08-09 14:32:57 -0400

    added in handlebars files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var method-override = require('method-override');
+var body-parser = require('body-parser');
+var router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
